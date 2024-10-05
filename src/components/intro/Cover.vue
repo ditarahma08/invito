d7038e3c7a8003ed4fc50fcdfad2ab3e66ac5dd0
ditarahma08 <ditarahma08@gmail.com>
2024-10-05 19:52:04 +0700

    add hero page

---
+++ src/components/intro/Cover.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="cover-invitation">
+    <h1>29 . 01 . 2025</h1>
+    <img src="@/assets/images/img_hero_featured.jpg" />
+
+    <div class="cover-invitation__recipient">
+      <span>Dear Guest</span>
+      <button type="button">Open Invitation</button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.cover-invitation {
+  @apply relative;
+  @apply h-full flex flex-col items-center justify-items-center;
+
+  font-family: "Lora";
+}
+
+.cover-invitation h1 {
+  @apply text-6xl fixed top-[45%];
+  @apply md:text-7xl;
+}
+
+.cover-invitation img {
+  @apply overflow-hidden object-cover;
+  @apply h-[420px] w-[295px];
+  @apply mt-auto mb-auto;
+}
+
+.cover-invitation__recipient {
+  @apply absolute flex flex-col items-center gap-2;
+  @apply text-lg;
+
+  bottom: calc(50% - 325px);
+}
+
+.cover-invitation__recipient button {
+  @apply py-2 px-4 rounded-lg;
+  @apply hover:shadow-md;
+
+  color: var(--color-background-soft);
+  background-color: var(--color-text);
+}
+</style>
---
+++ src/views/IntroView.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+import Cover from "@/components/intro/Cover.vue";
+</script>
+
+<template>
+  <div class="first">
+    <div class="hero">
+      <div class="header">
+      <h1 class="header__title">Dita & Danan</h1>
+    </div>
+
+      <div class="hero__section">
+        <Cover />
+      </div>
+
+      <div class="hero__section --right">
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.first {
+  @apply h-screen flex flex-col;
+}
+
+.header {
+  @apply fixed w-full;
+  @apply flex justify-center items-center;
+  @apply p-6;
+}
+
+.header__title {
+  @apply text-3xl font-bold;
+
+  font-family: "Lora";
+}
+
+.hero {
+  @apply flex w-full h-full;
+}
+
+.hero__section {
+  @apply w-full;
+}
+
+.hero__section.--right {
+  @apply bg-cover bg-bottom hidden;
+  @apply lg:block;
+
+  background-image: url(@/assets/images/img_hero_cover.jpg);
+}
+</style>

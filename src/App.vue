d40bd70d2fd85cd2bffc93d5f1e4f68a4492daf5
ditarahma08 <ditarahma08@gmail.com>
2024-10-05 16:02:01 +0700

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
+import HomeView from "./views/HomeView.vue";
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -15,13 +16,18 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
-  </header>
+  </header> -->
+  <HomeView />
 
-  <RouterView />
+  <!-- <RouterView /> -->
 </template>
 
 <style scoped>
-header {
+@font-face {
+  font-family: Lora;
+  src: url("~@/assets/lora-medium.ttf");
+}
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -81,5 +87,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="header">
+    <h1 class="header__title">
+      Dita & Danan
+    </h1>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  @apply fixed top-0 w-full;
+  @apply flex justify-center items-center;
+  @apply p-5;
+}
+
+.header__title {
+  @apply text-3xl font-bold text-neutral-200;
+  
+  font-family: 'Lora';
+}
+</style>
